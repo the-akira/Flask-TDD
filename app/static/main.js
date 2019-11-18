1ac06b6dccaa66ecc624003d530c097090635eb1
the-akira <gabrielfelippe90@gmail.com>
2019-11-17 21:06:34 -0300

    first commit

---
+++ app/static/main.js
@@ -0,0 +1,22 @@
+$(function() {
+  console.log('ready!'); // sanity check
+});
+
+$('.entry').on('click', function() {
+  var entry = this;
+  var post_id = $(this).find('h2').attr('id');
+  var result = confirm("Deseja mesmo remover este item?");
+  if(result){
+  $.ajax({
+    type:'GET',
+    url: '/delete' + '/' + post_id,
+    context: entry,
+    success:function(result) {
+      if(result.status === 1) {
+        $(this).remove();
+        console.log(result);
+      }
+    }
+  });
+ }
+});
